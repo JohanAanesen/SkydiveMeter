7f5fd9491d55b8c2c0bb9d7df7ddf4569590deb3
Johan Aanesen <johanaan@stud.ntnu.no>
2018-06-02 23:15:48 +0200

    gravity variable added

--- app/src/main/java/net/eksde/skydivemeter/HomeActivity.java
+++ app/src/main/java/net/eksde/skydivemeter/HomeActivity.java
@@ -13,17 +13,26 @@ import android.widget.TextView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.lang.reflect.Array;
+
 public class HomeActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager mSensorManager;
     private Sensor mPressure;
+    private Sensor mGravity;
+
     private TextView pressureText;
     private TextView pressureText2;
     private TextView pressureText3;
     private TextView baseLineText;
     private TextView baseLineText2;
     private TextView baseLineText3;
+    private TextView gravityText;
+
     private TextView altiText;
+    private TextView graviText;
+
+
     private int groundCounter = 900;
     private int airCounter = 0;
     private float baseLinePressure;
@@ -36,24 +45,31 @@ public class HomeActivity extends AppCompatActivity implements SensorEventListen
     private FirebaseDatabase database;
     private DatabaseReference myRef;
 
+    private double test = 20;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        //TextViews being initialized and variable mapped
         pressureText = (TextView) findViewById(R.id.pressureText);
         pressureText2 = (TextView) findViewById(R.id.pressureText2);
         pressureText3 = (TextView) findViewById(R.id.pressureText3);
         baseLineText = (TextView) findViewById(R.id.baseLineText);
         baseLineText2 = (TextView) findViewById(R.id.baseLineText2);
         baseLineText3 = (TextView) findViewById(R.id.baseLineText3);
+        gravityText = (TextView) findViewById(R.id.gravityText);
 
         altiText = (TextView) findViewById(R.id.altiText);
+        graviText = (TextView) findViewById(R.id.graviText);
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mPressure = mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        mGravity = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         altiText.setText("0.0 ft");
+        graviText.setText("0.0 M/S^2");
 
 
         // Write a message to the database
@@ -71,54 +87,66 @@ public class HomeActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public final void onSensorChanged(SensorEvent event) {
-        float pressure = event.values[0];
-        double meter = mSensorManager.getAltitude(SensorManager.PRESSURE_STANDARD_ATMOSPHERE, pressure);
-        double feet = meter / 0.3048;
-        pressureText.setText(String.format("%f" ,pressure) + " hPa / mBar");
-        pressureText2.setText(String.format("%f", feet) + " feet");
-        pressureText3.setText(String.format("%f", meter) + " meter");
-        // Do something with this sensor data.
+        if(event.sensor.getType() == Sensor.TYPE_PRESSURE) {
 
-        //rebase if the counter goes up
-        if (groundCounter >= 900){
-            baseLinePressure = pressure;
-            baseLineMeter = meter;
-            baseLineFeet = feet;
+            float pressure = event.values[0];
 
-            baseLineText.setText(String.format("%f" ,baseLinePressure) + " hPa / mBar");
-            baseLineText2.setText(String.format("%f", baseLineFeet) + " feet");
-            baseLineText3.setText(String.format("%f", baseLineMeter) + " meter");
-            groundCounter = 0;
-        }
+            double meter = mSensorManager.getAltitude(SensorManager.PRESSURE_STANDARD_ATMOSPHERE, pressure);
+            double feet = meter / 0.3048;
+            pressureText.setText(String.format("%.3f", pressure) + " hPa / mBar");
+            pressureText2.setText(String.format("%.3f", feet) + " feet");
+            pressureText3.setText(String.format("%.3f", meter) + " meter");
+            // Do something with this sensor data.
 
-        if (airCounter > 9000){
-            Log.d("SHIT BOI", "you fly");
-        }
+            //rebase if the counter goes up
+            if (groundCounter >= 900) {
+                baseLinePressure = pressure;
+                baseLineMeter = meter;
+                baseLineFeet = feet;
 
-        if(airCounter > 0 && (airCounter % 5) == 0) {
-
-            if(boot){
-                sessionKey = myRef.push().getKey();
-                boot = false;
+                baseLineText.setText(String.format("%.3f", baseLinePressure) + " hPa / mBar");
+                baseLineText2.setText(String.format("%.3f", baseLineFeet) + " feet");
+                baseLineText3.setText(String.format("%.3f", baseLineMeter) + " meter");
+                groundCounter = 0;
             }
 
-            //altitude relative to the base
-            Double altitude = Math.floor(feet - baseLineFeet);
-            altiText.setText(Double.toString(altitude) + " ft");
-
-            if(sessionKey != null && (airCounter % 50) == 0) {
-                myRef.child(sessionKey).child(Integer.toString(airCounter / 5)).setValue(altitude);
+            if (airCounter > 9000) {
+                Log.d("SHIT BOI", "you fly");
             }
-        }
 
-        if(meter < baseLineMeter+20){
-            airCounter = 0;
-            groundCounter++;
-        }else{
-            groundCounter = 0;
-            airCounter++;
-        }
+            if (airCounter > 0 && (airCounter % 5) == 0) {
 
+                if (boot) {
+                    sessionKey = myRef.push().getKey();
+                    boot = false;
+                }
+
+                //altitude relative to the base
+                Double altitude = Math.floor(feet - baseLineFeet);
+                altiText.setText(Double.toString(altitude) + " ft");
+
+                if (sessionKey != null && (airCounter % 50) == 0) {
+                    myRef.child(sessionKey).child(Integer.toString(airCounter / 5)).setValue(altitude);
+                }
+            }
+
+            if (meter < baseLineMeter + 20) {
+                airCounter = 0;
+                groundCounter++;
+            } else {
+                groundCounter = 0;
+                airCounter++;
+            }
+
+        }else if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            double gravity = Math.sqrt(Math.pow(event.values[0], 2) + Math.pow(event.values[1], 2) + Math.pow(event.values[2], 2));
+
+
+            if(gravity < test){test = gravity;}
+
+            gravityText.setText(Double.toString(test) + " M/S^2");
+            graviText.setText(String.format("%.2f", gravity) + " M/S^2");
+        }
     }
 
     @Override
@@ -126,6 +154,7 @@ public class HomeActivity extends AppCompatActivity implements SensorEventListen
         // Register a listener for the sensor.
         super.onResume();
         mSensorManager.registerListener(this, mPressure, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mGravity, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     @Override
@@ -133,8 +162,11 @@ public class HomeActivity extends AppCompatActivity implements SensorEventListen
         // Be sure to unregister the sensor when the activity pauses.
         super.onPause();
         mSensorManager.unregisterListener(this);
+        mSensorManager.unregisterListener(this);
     }
 
+
+    //legacy mBar/pha to feet formula
     public double pressureToFeet(float pressure){
         //Altitude = (10^(log(P/P_0)/5.2558797)-1/(-6.8755856*10^-6)
 
